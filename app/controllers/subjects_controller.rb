ec31bbf6e983ed78d5ab48477801bfeb389b93c5
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-08-13 15:34:58 +0000

    Add flash messages after creation and update

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -5,6 +5,7 @@ class SubjectsController < ApplicationController
 
   def show
     @subject = Subject.find(params[:id])
+
   end
 
   def new
@@ -14,6 +15,7 @@ class SubjectsController < ApplicationController
   def create
     @subject = Subject.new(subjects_params);
     if @subject.save
+      flash[:notice] = "Subject created successfully"
       redirect_to(subjects_path)
     else
       render 'new'
@@ -21,15 +23,28 @@ class SubjectsController < ApplicationController
   end
 
   def edit
+    @subject = Subject.find(params[:id])
   end
 
   def update
+    @subject = Subject.find(params[:id]);
+    if @subject.update_attributes(subjects_params)
+      flash[:notice] = "Subject updated successfully"
+      redirect_to(subject_path(@subject))
+    else
+      render 'edit'
+    end
   end
 
   def delete
+    @subject = Subject.find(params[:id])
   end
 
   def destroy
+    @subject = Subject.find(params[:id])
+    @subject.destroy
+    flash[:notice] = "Subject '#{@subject.name}' destroyed successfully"
+    redirect_to(subjects_path)
   end
   private
   def subjects_params
