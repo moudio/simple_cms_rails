6cf4d276bccbc6bbd253a4a3405ccd10d1e5f80b
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-08-13 15:59:56 +0000

    Create the logic for page creation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,11 +4,19 @@ class PagesController < ApplicationController
   end
 
   def new
-    @page = Page.new 
+    @page = Page.new
   end
 
 
   def create
+    @page = Page.new(page_params)
+    if @page.save
+      flash[:notice] = "Page created successfully"
+      redirect_to(pages_path)
+    else
+
+      render 'new'
+    end
   end
 
   def show
@@ -19,5 +27,10 @@ class PagesController < ApplicationController
   end
 
   def delete
+  end
+
+  private
+  def page_params
+    params.require(:page).permit(:name, :permalink, :position, :visible)
   end
 end
