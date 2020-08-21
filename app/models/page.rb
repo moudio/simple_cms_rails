cd641b8b2567d3485fc78da2128184d91b5df00d
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-08-21 11:20:26 +0000

    Add validations to models

--- app/models/page.rb
+++ app/models/page.rb
@@ -9,4 +9,9 @@ class Page < ApplicationRecord
   scope :sorted, lambda { order("position ASC")}
   scope :newest_first, lambda { order("created_at DESC")}
 
+  validates_presence_of :name
+  validates_length_of :name, :maximum => 255
+  validates_presence_of :permalink
+  validates_length_of :permalink, :within => 3...255
+  validates_uniqueness_of :permalink
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -2,4 +2,12 @@ class Section < ApplicationRecord
   belongs_to :page
   has_many :section_edits
   has_many :admin_users, through: :section_edits
+
+
+  CONTENT_TYPES = ['text', 'HTML']
+  validates_presence_of :name
+  validates_length_of :name, :maximum => 255
+  validates_inclusion_of :content_type, :in => CONTENT_TYPES,
+  :message => "must be one of: #{CONTENT_TYPES.join(', ')}"
+  validates_presence_of :content
 end
